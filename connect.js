dd2177b1a50686ca2f025b05b86e91eb9a43c01c
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 15:10:14 +0000

    added ..something

---
+++ connect.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Liam on 23/11/2015.
+ */
+
+// lets require/import the mongodb native drivers.
+var mongodb = require("mongodb");
+
+// and the HTTP Server
+var http = require("http");
+
+// Setup the port
+var port = "process.env.PORT";
+
+// url to our database
+var url = 'mongodb://liambowie:dbpass123@ds054298.mongolab.com:54298/db_1206499;';
+
+// MongoClient allows us to actually talk to the database
+var mongoClient = mongodb.MongoClient;
+
+http.createServer(function(request, response){
+    response.writeHead(200 {'Content-Type': 'text/plain'});
+    response.write('Connecting\n');
+
+    // Use the connect method to connect to the server
+    MongoClient.connect(url, function(err, db){
+        response.write('Connection Made\n');
+        if(err)
+        {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            // If an error occurs, close the db
+            db.close();
+        }
+        else
+        {
+            // Yay we're connected
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database
+
+            // Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection Closed');
+    });
+}).listen(port);
+
+
