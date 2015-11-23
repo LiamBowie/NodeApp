03e6d89d36a847f5dec00945c7220a1e63746879
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 14:24:21 +0000

    Back to Hello World

--- server.js
+++ server.js
@@ -7,18 +7,8 @@ var port = 1337;
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, { 'Content-Type': 'text/plain'});
-    response.write("<!DOCTYPE 'html'>");
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello World Page</title>");
-    response.write("</head>");
-    response.write("<body>");
-    response.write("<h1>");
-    response.write("Hello World!");
-    response.write("</h1>");
-    response.write("</body>");
-    response.write("</html>");
-    response.end();
+
+    response.end('Hello World\n');
 });
 server.listen(port);
 console.log("Server is Listening!");
