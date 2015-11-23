63c6713ec7fe6bdc453dba187d77be7db4f6e32a
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 14:26:18 +0000

    Changed port again

--- server.js
+++ server.js
@@ -3,12 +3,22 @@
  */
 
 var http = require('http');
-var port = 1337;
+var port = process.env.PORT;
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, { 'Content-Type': 'text/plain'});
-
-    response.end('Hello World\n');
+    response.write("<!DOCTYPE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h1>");
+    response.write("Hello World!");
+    response.write("</h1>");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
 });
 server.listen(port);
 console.log("Server is Listening!");
