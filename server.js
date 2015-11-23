515e995c05de8c85cbd30de5bf1844f7105e0ac5
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 14:15:15 +0000

    Writing HTML with Node.js

--- server.js
+++ server.js
@@ -7,5 +7,16 @@ var port = 1337;
 
 http.createServer(function(request, response){
     response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.write("<!DOCTYPE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h1>");
+    response.write("Hello World!");
+    response.write("</h1>");
+    response.write("</body>");
+    response.write("</html>");
     response.end('Hello World\n');
 }).listen(port);
