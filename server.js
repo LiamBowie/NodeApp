410501416b0cd2240f67d675e6909aa1a475ed40
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 14:27:58 +0000

    Changed Content-Type to HTML

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 var port = process.env.PORT;
 
 var server = http.createServer(function(request, response){
-    response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.writeHead(200, { 'Content-Type': 'text/html'});
     response.write("<!DOCTYPE 'html'>");
     response.write("<html>");
     response.write("<head>");
