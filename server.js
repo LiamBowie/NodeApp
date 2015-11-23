63016abb80ec1ce9f3908826f4f85a9d43d53459
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-23 14:08:58 +0000

    Added code to server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 /**
  * Created by Liam on 23/11/2015.
  */
+
+var http = require('http');
+var port = process.env.PORT;
+
+http.createServer(function(request, response){
+    response.writeHead(200, { 'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(port);
