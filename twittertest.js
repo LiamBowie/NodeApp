7c085d48e0e2f0b8e46e823246e6aeb325edc222
1206499 <1206499@rgu.ac.uk>
2015-11-30 14:38:45 +0000

    Added twittertest.js, updated package.json and and server.js

---
+++ twittertest.js
@@ -0,0 +1,19 @@
+var Twitter = require('twitter');
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+
+var client = new Twitter({
+    consumer_key: 'SaQghvtQxMloQiy2y2Og1WhHz',
+    consumer_secret: 'gRcx84mCrxGrZf1lQovIMUIBxnlKMWUny6RFF4sBnse7C6sB5K',
+    access_token_key: '4113750323-c5gEo7FZbLyanCutSJVZGi2BBU4iuVsBGkEDpTl',
+    access_token_secret: 'B0keLKWJTrmfsK11hz9lPL8n0sejAVI8m5UJCkrrGhhMt'
+});
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    client.get('search/tweets', {q: 'lolcat'}, function (error, tweets) {
+        console.log(tweets)
+    });
+}).listen(port);
